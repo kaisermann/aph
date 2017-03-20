a60b0385aa4a22217fd31c8811470a565b9d99cb
Christian Kaisermann <christian@kaisermann.me>
2017-03-20 15:43:22 -0300

    Removed some helper functions from the prototype

--- dist/aph.js
+++ dist/aph.js
@@ -7,9 +7,10 @@
 var Apheleia = function Apheleia (elems, contextOrAttr, nothingOrAttrVal) {
   // Second parameter is used as context when a HTML Element is passed
   // Second/Third parameter are used as attribute object/list/pair when creating elements
-  this.context = contextOrAttr instanceof Element ? contextOrAttr : document;
-  this.elements = this.parseElems(elems);
-  this.length = this.elements.length;
+
+  this.elements = aphParseElements(elems,
+    this.context = aphParseContext(contextOrAttr)
+  );
 
   // If second parameter is not an html element and not undefined, we assume it's an attribute obj
   if (!(contextOrAttr instanceof Element) && contextOrAttr) {
@@ -17,34 +18,13 @@ var Apheleia = function Apheleia (elems, contextOrAttr, nothingOrAttrVal) {
   }
 };
 
-Apheleia.prototype.parseElems = function parseElems (stringOrListOrNode) {
-  // If single string
-  if ('' + stringOrListOrNode === stringOrListOrNode) {
-    var isCreationStr = /<(\w*)\/?>/.exec(stringOrListOrNode);
-    // If creation string
-    if (isCreationStr) {
-      return [document.createElement(isCreationStr[1])]
-    }
-    // If not a creation string, let's search for the elements
-    return Array.prototype.slice.call(this.context.querySelectorAll(stringOrListOrNode))
-  }
-  // If single node
-  if (stringOrListOrNode instanceof Element) {
-    return [stringOrListOrNode]
-  }
-  // If node list
-  if (NodeList.prototype.isPrototypeOf(stringOrListOrNode)) {
-    return Array.prototype.slice.call(stringOrListOrNode)
-  }
-  // If array, we're done
-  if (Array.isArray(stringOrListOrNode)) {
-    return stringOrListOrNode
-  }
-  return []
+Apheleia.prototype.filter = function filter (cb) {
+  return new Apheleia(this.elements.filter(cb))
 };
 
 Apheleia.prototype.get = function get (index) {
-  return index !== undefined ? this.elements[index] : this.elements
+  // Type coercion uses less bytes than "index !== undefined"
+  return +index === index ? this.elements[index] : this.elements
 };
 
 // Iterates through the elements with a 'callback(element, index)''
@@ -61,6 +41,7 @@ Apheleia.prototype.attr = function attr (objOrKey, nothingOrValue, prepend) {
 
   var tmpObj = objOrKey;
   // Is the first parameter a key string?
+  // Type coercion uses less bytes than "typeof objOrKey ==='string'"
   if ('' + objOrKey === objOrKey) {
     // If passed only a key, let's return the attribute
     if (nothingOrValue === undefined) {
@@ -72,15 +53,15 @@ Apheleia.prototype.attr = function attr (objOrKey, nothingOrValue, prepend) {
   }
 
   // Finally, let's set the attributes
-  this.each(function (elem) { return Object.keys(tmpObj).forEach(function (key) { return elem.setAttribute(prepend + key, tmpObj[key]); }
+  return this.each(function (elem) { return Object.keys(tmpObj).forEach(function (key) { return elem.setAttribute(prepend + key, tmpObj[key]); }
     ); }
-  );
-  return this
+  )
 };
 
 Apheleia.prototype.prop = function prop (objOrKey, nothingOrValue) {
   var tmpObj = objOrKey;
   // Is the first parameter a key string?
+  // Type coercion uses less bytes than "typeof objOrKey ==='string'"
   if ('' + objOrKey === objOrKey) {
     // If passed only a key, let's return the property
     if (nothingOrValue === undefined) {
@@ -92,20 +73,14 @@ Apheleia.prototype.prop = function prop (objOrKey, nothingOrValue) {
   }
 
   // Finally, let's set the properties
-  this.each(function (elem) { return Object.keys(tmpObj).forEach(function (key) {
+  return this.each(function (elem) { return Object.keys(tmpObj).forEach(function (key) {
       elem[key] = tmpObj[key];
     }); }
-  );
-  return this
+  )
 };
 
 Apheleia.prototype.data = function data (objOrKey, nothingOrValue) {
   return this.attr(objOrKey, nothingOrValue, 'data-')
-};
-
-Apheleia.prototype.filter = function filter (cb) {
-  return new Apheleia(this.elements.filter(function (elem) { return cb(elem); }
-  ))
 };
 
 Apheleia.prototype.appendTo = function appendTo (newParent) {
@@ -158,7 +133,47 @@ Apheleia.prototype.once = function once (events, cb) {
     var this$1 = this;
 
   var onceFn = function (e) { return (cb(e) || this$1.off(e.type, onceFn)); };
-  this.on(events, onceFn);
+  return this.on(events, onceFn)
+};
+
+// "Private" Helpers. No need to keep this in the prototype.
+var aphParseContext = function (contextOrAttr) {
+  return contextOrAttr instanceof Element
+    ? contextOrAttr // If already a html element
+    : Apheleia.prototype.isPrototypeOf(contextOrAttr)
+      ? contextOrAttr.get(0) // If already apheleia object
+      : document // Probably an attribute was passed. Return the document.
+};
+
+var aphParseElements = function (stringOrListOrNode, ctx) {
+  // If single string
+  // Type coercion uses less bytes than "typeof stringOrListOrNode ==='string'"
+  if ('' + stringOrListOrNode === stringOrListOrNode) {
+    var isCreationStr = /<(\w*)\/?>/.exec(stringOrListOrNode);
+    // If creation string
+    if (isCreationStr) {
+      return [document.createElement(isCreationStr[1])]
+    }
+    // If not a creation string, let's search for the elements
+    return Array.prototype.slice.call(ctx.querySelectorAll(stringOrListOrNode))
+  }
+  // If single node
+  if (stringOrListOrNode instanceof Element) {
+    return [stringOrListOrNode]
+  }
+  // If node list
+  if (NodeList.prototype.isPrototypeOf(stringOrListOrNode)) {
+    return Array.prototype.slice.call(stringOrListOrNode)
+  }
+  // If array, we're done
+  if (Array.isArray(stringOrListOrNode)) {
+    return stringOrListOrNode
+  }
+  // If another apheleia object is passed, get all elements from it
+  if (Apheleia.prototype.isPrototypeOf(stringOrListOrNode)) {
+    return stringOrListOrNode.get()
+  }
+  return []
 };
 
 function aph (elems, contextOrAttrKey, nothingOrAttrVal) {
